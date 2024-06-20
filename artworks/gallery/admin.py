aecdebe0d564e47bbbd0fbdba083bc2ad0277e43
Paula De Amicis <paula_dea@live.com>
2024-06-20 21:00:40 +1000

    Registered models in admin site

--- artworks/gallery/admin.py
+++ artworks/gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Artist, Movement, Artwork
+
+admin.site.register(Artist)
+admin.site.register(Movement)
+admin.site.register(Artwork)
+
 
 # Register your models here.
