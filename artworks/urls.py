76f1b7c59e7a24a3f1c8d2f30f1fd255eb811c8f
Paula De Amicis <paula_dea@live.com>
2024-06-25 20:48:26 +1000

    Syntax updates following debugging

--- artworks/urls.py
+++ artworks/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('movement/<str:movement>/', views.search_by_movement, name='search_by_movement'),
     path('artist/<int:artist_id>/', views.artist_detail, name='artist_detail'),
-    path('artists/', views.artists_list, name='artist_list'),
+    path('artists/', views.artists_list, name='artists_list'),
     path('movements/', views.movements_list, name='movements_list'),
 ]
