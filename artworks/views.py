37d9fb015e4f991f8372696d8a9d1368935af96b
Paula De Amicis <paula_dea@live.com>
2024-06-25 14:50:11 +1000

    Updates to views.py
    new views: artist_list and movement_list will match upcoming new html pages

--- artworks/views.py
+++ artworks/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Artist, Movement, Artwork
+import random
+
 
 def home(request):
     artworks = Artwork.objects.all()
     return render(request, 'gallery/home.html', {'artworks': artworks})
+
 
 def search_by_movement(request, movement):
     # Fetch the movement object based on the provided movement name.
@@ -13,10 +16,20 @@ def search_by_movement(request, movement):
     artworks = Artwork.objects.filter(movement=movement_obj)
 
     # Render the 'movement.html' template with the context containing the artworks and movement object.
-    return render(request, 'gallery/movement.html', {'artworks':artworks, 'movement': movement_obj})
+    return render(request, 'gallery/movement.html', {'artworks': artworks, 'movement': movement_obj})
+
 
 def artist_detail(request, artist_id):
     artist = get_object_or_404(Artist, id=artist_id)
     artworks = Artwork.objects.filter(artist=artist)
     return render(request, 'gallery/artist_detail.html', {'artist': artist, 'artworks': artworks})
 
+def artists_list(request):
+    artists = list(Artist.objects.all())
+    random.shuffle(artists)
+    return render(request, 'gallery/artists_list.html', {'artists': artists})
+
+def movements_list(request):
+    movements = list(Movement.objects.all())
+    random.shuffle(movements)
+    return render(request, 'gallery/movements_list.html', {'movements' : movements})
