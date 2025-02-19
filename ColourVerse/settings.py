15111114dd43053ca540cadb4abdeaed7327b331
paulad8 <paula_dea@live.com>
2025-02-19 21:41:21 +1000

    New configs to deploy to  Heroku

--- ColourVerse/settings.py
+++ ColourVerse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,10 +78,7 @@ WSGI_APPLICATION = "ColourVerse.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
