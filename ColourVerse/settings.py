b72135ae8ebe8b07c775b1b326629695386d2b06
paulad8 <paula_dea@live.com>
2025-02-19 22:03:02 +1000

    Removed AWS storage configs

--- ColourVerse/settings.py
+++ ColourVerse/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -112,21 +113,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-# AWS S3 Configuration for Static and Media files
-AWS_STORAGE_BUCKET_NAME = 'art-space-static-files'
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_ACCESS_KEY_ID = 'AKIA3LET5Q4PJ3ZWJHC5'
-AWS_SECRET_ACCESS_KEY = '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'
+
 
 # Static files
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Media files (optional)
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'artists')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
